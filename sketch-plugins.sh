6ab63a8d270981f164038656ce64cb25b60a3f6a
Paul Farino <pivotal@Pivotals-MacBook-Pro.local>
2015-10-08 15:06:40 -0400

    Initial Commit

---
+++ sketch-plugins.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# For Sketch Downloaded via the Internet
+
+clear
+echo "Installing all Sketch Plugins onto your Mac"
+echo "Downloading the sketch plugins..."
+cd ~/Documents/
+curl -L https://github.com/paulfarino/Sketch-Shell/tarball/master -o master.tar.gz
+echo "Extracting the plugins..."
+mkdir -p sketch-plugins/plugins
+tar -zxf master.tar.gz -C sketch-plugins/plugins
+cd sketch-plugins
+cd plugins
+find . -mindepth 2 -type f -print -exec mv {} . \;
+rm -R -- */
+rm *.txt
+rm *.json
+rm *.csv
+rm *.md
+rm *.html
+rm *.py
+rm AUTHORS
+rm CONTRIBUTORS
+cd ..
+mv plugins/* /Library/Application Support/com.bohemiancoding.sketch3/Plugins
+
+echo "Plugins installed; Cleaning up files..."
+cd ~/Documents/
+rm -f master.tar.gz
+rm -rf sketch-plugins
+echo "All done! All Sketch Plugins installed."
